ed94b02031dc604f689264037feeeb5aa1ee1540
Long Bá <lebalong12d2@gmail.com>
2023-04-12 01:01:00 +0700

    first commit

---
+++ OrderManager.java
@@ -0,0 +1,45 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class OrderManager {
+    private Map<Integer, Order> orderMap; 
+    private DataAccess dataAccess;
+
+    public OrderManager() {
+        orderMap = new HashMap<>();
+        dataAccess = new DataAccess();
+        orderMap = dataAccess.loadOrders(); 
+    }
+
+    public void addOrder(Order order) {
+        int orderId = order.getId();
+        orderMap.put(orderId, order);
+        dataAccess.saveOrders(orderMap); 
+    }
+
+    public void updateOrder(Order order) {
+        int orderId = order.getId();
+        if (orderMap.containsKey(orderId)) {
+            orderMap.put(orderId, order);
+            dataAccess.saveOrders(orderMap); 
+        } else {
+            System.out.println("Không tìm thấy đơn hàng có mã " + orderId);
+        }
+    }
+
+    public void deleteOrder(int orderId) {
+        if (orderMap.containsKey(orderId)) {
+            orderMap.remove(orderId);
+            dataAccess.saveOrders(orderMap); 
+        } else {
+            System.out.println("Không tìm thấy đơn hàng có mã " + orderId);
+        }
+    }
+
+    public void displayAllOrders() {
+        System.out.println("Danh sách đơn hàng:");
+        for (Order order : orderMap.values()) {
+            System.out.println(order);
+        }
+    }
+}
